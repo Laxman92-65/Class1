297a988d160bb86581cfd47e670d7de294304435
Laxman92-65 <laxmanmarothi.92@gmail.com>
2025-07-10 23:52:21 +0530

    Add files via upload
    "Initial commit"

---
+++ Laxman_exercise1.py
@@ -0,0 +1,170 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "35a8f615-fb4d-4fb3-a007-057ad2fecbce",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "sachin t-shirt number is 10\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Sample Question print - sachin t - shirt number is 10\n",
+    "name = \"sachin\"\n",
+    "t_shirt = 10\n",
+    "# Your Answer\n",
+    "\n",
+    "print(name,\"t-shirt number is\",t_shirt)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "7cc01539-5757-42b5-9dad-a8a41277a3b4",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "india is number 1 team and has won 2012 worldcup and it rank 1 and the captain name is kholi\n"
+     ]
+    }
+   ],
+   "source": [
+    "# 1 . Question | print - India is number 1 team and has won 2012 worldcup and it rank 1 and the captain name is kholi\n",
+    "team = \"india\"\n",
+    "worldcup = 2012\n",
+    "captain = \"kholi\"\n",
+    "rank = 1\n",
+    "print(team,\"is number 1 team and has won\",worldcup,\"worldcup and it rank\",rank,\"and the captain name is\",captain)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "5a80f35d-62fd-4ed3-847b-d6f189f69dd3",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "<class 'int'>\n",
+      "<class 'float'>\n",
+      "<class 'str'>\n"
+     ]
+    }
+   ],
+   "source": [
+    "# 2 . Question |print the datatype of the x,y,z\n",
+    "\n",
+    "x = 56\n",
+    "y = 56.78\n",
+    "z = \"python\"\n",
+    "print(type(x))\n",
+    "print(type(y))\n",
+    "print(type(z))\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "id": "817fe749-1cd1-4aa7-aa2e-a2056342f5e4",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "10.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "# 3 . Question | write a program to print 10.0 using typecasting\n",
+    "a = 10\n",
+    "print(float(a))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 13,
+   "id": "84ff2699-ee69-420f-b652-678274e5fd3d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "12\n"
+     ]
+    }
+   ],
+   "source": [
+    "# 4 . Question | write a program to print 12 using typecasting\n",
+    "\n",
+    "a = 12.56\n",
+    "print(int(a))\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "id": "00001dc9-982d-4b30-9411-96fb9be3054d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "30\n"
+     ]
+    }
+   ],
+   "source": [
+    "# 5 . write a program print 30 by using typecasting conncept\n",
+    "\n",
+    "a = \"10\"\n",
+    "b = \"20\"\n",
+    "c = int(a) + int(b)\n",
+    "print(c)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6d561f48-9313-4e52-9896-a340c63729a3",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
